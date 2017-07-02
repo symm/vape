bd9b5af5076e28ebeaddb6ed33d81466dd569f7b
Gareth Jones <me@gazj.co.uk>
2017-07-02 17:41:14 +0100

    Print timing and result info

--- main.go
+++ main.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
+var vapeFile = flag.String("config", "Vapefile", "The full path to the Vape configuration file")
+
 func main() {
-	var vapeFile = flag.String("config", "Vapefile", "The full path to the Vape configuration file")
+	start := time.Now()
+
 	flag.Parse()
 
 	args := flag.Args()
@@ -33,12 +37,26 @@ func main() {
 	vape := NewVape(DefaultClient, baseURL, resCh, errCh)
 	vape.Process(smokeTests)
 
+	passedCount := 0
+
 	for i := 0; i < testsLen; i++ {
 		select {
 		case res := <-resCh:
+			if res.Passed() {
+				passedCount++
+			}
+
 			fmt.Println(formatResult(res))
 		case err := <-errCh:
 			fmt.Println(err)
 		}
 	}
+
+	if passedCount < testsLen {
+		fmt.Println("\n🔥  Some tests failed. you may have a bad deployment")
+		os.Exit(2)
+	}
+
+	fmt.Printf("\n✨  [%d/%d] tests passed in %s\n", passedCount, testsLen, time.Since(start))
+
 }
