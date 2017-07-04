3b7e599c93a93e1747c83f5bab6736ded9e59693
Gareth Jones <me@gazj.co.uk>
2017-07-04 22:28:36 +0100

    Add default transport

--- http.go
+++ http.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"net/http"
 	"time"
 )
@@ -10,7 +11,15 @@ type HTTPClient interface {
 	Get(url string) (*http.Response, error)
 }
 
+var defaultTransport = &http.Transport{
+	// TODO: allow this to be toggled
+	// So we can still smoke test sites with bad ssl cert
+	// e.g. ./vape https://self-signed.badssl.com/
+	TLSClientConfig: &tls.Config{InsecureSkipVerify: false},
+}
+
 // DefaultClient returns a HTTP client with configured timeouts.
 var DefaultClient = &http.Client{
-	Timeout: time.Duration(5 * time.Second),
+	Timeout:   time.Duration(5 * time.Second),
+	Transport: defaultTransport,
 }
