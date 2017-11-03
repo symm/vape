609d176a018331bee42351fb726afd8495a925f4
matthewdooler <matthew.dooler@sainsburys.co.uk>
2017-11-03 10:01:02 +0000

    Remove Get from HTTPClient interface

--- http.go
+++ http.go
@@ -8,7 +8,6 @@ import (
 
 // HTTPClient is a custom interface that wraps the basic Get method.
 type HTTPClient interface {
-	Get(url string) (*http.Response, error)
 	Do(req *http.Request) (*http.Response, error)
 }
 
